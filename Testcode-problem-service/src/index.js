92e3c89ecedf2a225cf30720042ff35e8f53ddf8
surjeetvermaiitm <surjeetverma2017@gmail.com>
2024-03-19 22:49:47 +0530

    problem service added

--- Testcode-problem-service/src/index.js
+++ Testcode-problem-service/src/index.js
@@ -1,21 +1,26 @@
-//process.loadEnvFile() // no need for dotenv package if u use this for updated node 
+const express = require('express');
+const bodyParser = require('body-parser');
 
-const { ServerConfig, Logger } = require("./config")
-const express = require("express");
+const { PORT } = require('./config/server.config');
+const apiRouter = require('./routes');
+
+
 const app = express();
 
-
-app.use(express.json());
-app.use(express.urlencoded({extended:true}));
-
-app.get("/ping",(req,res)=>{
-    return res.json({message:"Testcode problem service working"});
-})
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.text());
 
 
-app.listen(ServerConfig.PORT, () => {
-  console.log(`Server is up and running on PORT: ${ServerConfig.PORT}`);
-  
+// If any request comes and route starts with /api, we map it to apiRouter
+app.use('/api', apiRouter);
+
+
+app.get('/ping', (req, res) => {
+    return res.json({message: 'Problem Service is alive'});
 });
 
 
+app.listen(PORT, () => {
+    console.log(`Server started at PORT: ${PORT}`);
+});
